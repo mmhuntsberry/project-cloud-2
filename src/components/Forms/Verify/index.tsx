4d1d4cf2d50ff109fe3959ee3b3c85647f088ba0
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-02-25 10:18:26 -0700

    Style: Verify form

--- src/components/Forms/Verify/index.tsx
+++ src/components/Forms/Verify/index.tsx
@@ -1,44 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Form, TextInput, Button, Link } from "carbon-components-react";
-import { ArrowRight32 } from "@carbon/icons-react";
+import { RegisterContext } from "../../../contexts/RegisterContext";
+
+import styles from "./index.module.scss";
 
 export const Verify = () => {
+  const context = useContext(RegisterContext);
+  const { email } = context;
   const [code, setCode] = useState("");
   const SECRET: string = "1234567";
 
-  useEffect(() => {}, [code]);
+  useEffect(() => {
+    console.log("EMAIL", email);
+  }, [code, email]);
 
   const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setCode(evt.target.value);
   };
 
   return (
-    <Form className="form form--register">
-      <TextInput
-        className="u-margin-b-05"
-        id="verify-email"
-        invalidText="Invalid error message."
-        labelText="Verification code"
-        placeholder="Enter code"
-        type="text"
-        onChange={handleChange}
-      />
+    <>
+      <p className={styles.formDetails}>
+        For security we need to verify your identity. We sent a 7-digit code to{" "}
+        <strong>{email.value}</strong>. This code is valid for 30 minutes.
+      </p>
+      <Form className={`${styles.formContainer}`}>
+        <TextInput
+          className="u-margin-b-05"
+          id="verify-email"
+          invalidText="Invalid error message."
+          labelText="Verification code"
+          placeholder="ex. 1234567"
+          type="text"
+          size="xl"
+          light
+          onChange={handleChange}
+        />
 
-      <Button
-        disabled={code !== SECRET}
-        className="form__button"
-        renderIcon={ArrowRight32}
-      >
-        Next
-      </Button>
-      <div className="form-header__label-container form-header__label-container--verify">
-        <p className={`form-header__label u-margin-0 u-margin-r-02`}>
-          Didn't get an email?
-        </p>
-        <Link href="#" className="bx--link">
-          Resend email
-        </Link>
-      </div>
-    </Form>
+        <Button disabled={code !== SECRET} className={styles.formButton}>
+          Next
+        </Button>
+        <div className={`${styles.formLabelContainer} u-margin-t-05`}>
+          <p className={`${styles.formLabel} u-margin-0 u-margin-r-02`}>
+            Didn't get an email?
+          </p>
+          <Link href="#" className="bx--link">
+            Resend code
+          </Link>
+        </div>
+      </Form>
+    </>
   );
 };
