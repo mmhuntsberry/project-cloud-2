46efa03aa366ffa563c3249462aa5fba999bc8d0
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-03-03 12:10:08 -0700

    Style: Adjust sidebar font size and spacingg

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,33 +3,30 @@ import styles from "./index.module.scss";
 
 export const Sidebar = () => {
   return (
-    <ul
-      // className={`${styles.sidebar} bx--offset-lg-1 bx--col-lg-5 bx--col-md-5 bx--col-sm-4`}
-      className={`${styles.sidebar} bx--offset-lg-3 bx--col-lg-6`}
-    >
+    <ul className={`${styles.sidebar} bx--offset-lg-3 bx--col-lg-5`}>
       <li>
-        <h3 className={`${styles.sidebarHeading} u-margin-b-04`}>
+        <h3 className={`${styles.sidebarHeading} u-margin-b-03`}>
           No up-front changes
         </h3>
         <p
-          className={`${styles.sidebarSubheading} u-margin-b-07 sidebar__body--long`}
+          className={`${styles.sidebarSubheading} u-margin-b-05 sidebar__body--long`}
         >
           You will not be charged unless you manually upgrade to a paid account.
         </p>
       </li>
       <li>
-        <h3 className={`${styles.sidebarHeading} u-margin-b-05`}>
+        <h3 className={`${styles.sidebarHeading} u-margin-b-03`}>
           Security first
         </h3>
         <p
-          className={`${styles.sidebarSubheading} u-margin-b-07 sidebar__body--long`}
+          className={`${styles.sidebarSubheading} u-margin-b-05 sidebar__body--long`}
         >
           To make our platform more secure and prevent fraud, we verify everyone
           upfront.
         </p>
       </li>
       <li>
-        <h3 className={`${styles.sidebarHeading} u-margin-b-05`}>
+        <h3 className={`${styles.sidebarHeading} u-margin-b-03`}>
           $200 credit when you upgrade
         </h3>
         <p className={`${styles.sidebarSubheading}`}>
